62eb3bcc5e26764b593bbefbc8ea1bb85384cf02
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-11 21:09:06 +0300

    Add IRepository interface

---
+++ DogsApi/Data/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+    }
+}
