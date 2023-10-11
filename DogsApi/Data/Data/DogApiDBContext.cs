0a070c8b28fda6a63a09b6e620a1626e7a181c4c
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-11 21:09:42 +0300

    Add DogAPIDBContext

---
+++ DogsApi/Data/Data/DogApiDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Data
+{
+    public class DogAPIDBContext : DbContext
+    {
+        public DbSet<Dog> Dogs { get; set; }
+        public DogAPIDBContext(DbContextOptions<DogAPIDBContext> options, bool ensureDeleted = false) : base(options)
+        {
+            if (ensureDeleted)
+            {
+                Database.EnsureDeleted();
+            }
+            Database.EnsureCreated();
+        }
+
+    }
+}
