93f3fbc1adfb8c18fd01b9a42575eee71d64c3ad
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-11 21:10:22 +0300

    Add UnitOfWork

---
+++ DogsApi/Data/Data/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Data
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly DogAPIDBContext dbContext;
+
+        private DogRepository dogRepository;
+        public IRepository<Dog> DogRepository => dogRepository ??= new DogRepository(dbContext);
+
+        public UnitOfWork(DogAPIDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+    }
+}
