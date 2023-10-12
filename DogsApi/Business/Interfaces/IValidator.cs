d866d9de2803ec47e2c3400504b0dec51cdc588f
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-12 22:00:12 +0300

    Add IValidator

---
+++ DogsApi/Business/Interfaces/IValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Models;
+using Data.Interfaces;
+
+namespace Business.Interfaces
+{
+    public interface IValidator<TModel> where TModel : class
+    {        
+        public Task<ValidationResult> Validate(TModel model);
+        public void SetUnitOfWork(IUnitOfWork unitOfWork);
+    }
+}
