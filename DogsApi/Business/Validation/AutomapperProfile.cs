c3fdd54d43bf765caf8fe2f7202b8e57a6a2b081
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-12 22:01:46 +0300

    Add AutomapperProfile

---
+++ DogsApi/Business/Validation/AutomapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Business.Validation
+{
+    public class AutomapperProfile: Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Data.Entities.Dog, Models.DogModel>().ReverseMap();
+        }
+    }
+}
