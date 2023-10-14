5a00435a19fecd0a7a0272e998f78805217cb202
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-10-14 20:34:34 +0300

    Add RateLimitTest

---
+++ DogsApi/DogsApi.Tests/Integration.Tests/RateLimitTest.cs
@@ -0,0 +1,42 @@
+﻿using DogsApi.Tests.Utils;
+using System.Net;
+
+namespace DogsApi.Tests.Integration.Tests
+{
+    [TestClass]
+    public class RateLimitTest
+    {
+        private readonly CustomWebApplicationFactory<API.Startup> _factory;
+        private HttpClient _client;
+        private int rateLimitThreshold = 10;
+        public RateLimitTest()
+        {
+            _factory = new CustomWebApplicationFactory<API.Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task RateLimitExceedsLimit_Returns429StatusCode()
+        {
+            var responses = new List<HttpResponseMessage>();
+            for (int i = 0; i < rateLimitThreshold + 1; i++)
+            {
+                var response = await _client.GetAsync("/dogs");
+                responses.Add(response);
+            }
+
+            for (int i = 0; i < rateLimitThreshold + 1; i++) 
+            {
+                if (i < rateLimitThreshold)
+                {
+                    Assert.AreEqual(HttpStatusCode.OK, responses[i].StatusCode);
+                }
+                else 
+                {
+                    Assert.AreEqual(HttpStatusCode.TooManyRequests, responses[i].StatusCode);
+                }
+            }
+
+        }
+    }
+}
